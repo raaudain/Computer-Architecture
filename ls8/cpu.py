d7615233a1bbbc9318d2122edc6b9b4e63584039
Ramon Audain <raudain123@gmail.com>
2020-04-23 18:43:34 -0700

    incomplete

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -14,8 +14,11 @@ class CPU:
         self.prn = 0b01000111
         self.hlt = 0b00000001
         self.mul = 0b10100010
+        self.add = 0b10100000
         self.push = 0b01000101
         self.pop = 0b01000110
+        self.call = 0b01010000
+        self.ret = 0b00010001
         self.running = True
         self.branchtable = {
             self.ldi: self.handle_load_immediate,
@@ -23,10 +26,14 @@ class CPU:
             self.hlt: self.handle_halt,
             self.mul: self.handle_multiply,
             self.push: self.handle_push,
-            self.pop: self.handle_pop
+            self.pop: self.handle_pop,
+            self.call: self.handle_call,
+            self.ret: self.handle_return,
+            self.add: self.handle_add
         }
         self.sp = 7 # Stack Pointer
-        self.reg[self.sp] = 0xF4
+        self.reg[self.sp] = 0xFF # Used to keep register values between 0-255
+        
 
         
     def load(self, file):
@@ -106,9 +113,16 @@ class CPU:
         # Multiplies
         operand_a = self.ram_read(self.pc+1)
         operand_b = self.ram_read(self.pc+2)
-        #self.alu("MUL", operand_a, operand_b)
-        print(self.reg[operand_a] * self.reg[operand_b])
-        self.pc += 2
+        self.alu("MUL", operand_a, operand_b)
+        
+        self.pc += 3
+        
+    def handle_add(self):
+        operand_a = self.ram_read(self.pc+1)
+        operand_b = self.ram_read(self.pc+2)
+        self.alu("ADD", operand_a, operand_b)
+        
+        self.pc += 3
         
     def handle_push(self):
         # Decrement stack pointer
@@ -131,18 +145,55 @@ class CPU:
         
         # Copies value from memory into register
         reg_num = self.ram[self.pc+1]
-        self.reg[reg_num] = value # Being popped
+        self.reg[reg_num] = value # Being popped off of stack
         
         # Increment stack pointer
         self.reg[self.sp] += 1
         
         self.pc += 2
         
+    def handle_call(self):
+        # Computes return address
+        return_address = self.pc+2
+        
+        # Pushes to stack
+        self.reg[self.sp] -= 1
+        self.ram[self.reg[self.sp]] = return_address
+        
+        # Sets program counter to the value in the given register
+        register_number = self.ram[self.pc+1]
+        destination_address = self.reg[register_number]
+        
+        self.pc = destination_address
+        
+        print("REG", self.reg)
+        print("RAM", self.ram)
+        print("SP", self.sp)
+        print("Return address", return_address)
+        print("register_number", register_number)
+        print("destination_address", destination_address)
+    
+    def handle_return(self):
+        # Pop returns address from top of stack
+        return_address = self.ram[self.reg[self.pc]]
+        print("return",return_address)
+        self.reg[self.sp] += 1
+        
+        # Sets program counter
+        self.pc = return_address
+        
+        print("REG", self.reg)
+        print("RAM", self.ram)
+        print("SP", self.sp)
+        print("Return address", return_address)
+        # print("register_number", register_number)
+        # print("destination_address", destination_address)
+        
     def run(self):
         """Run the CPU."""
 
         while self.running:
-            # Internal Registers
+            # Instruction Register
             ir = self.ram_read(self.pc)
             
             if self.branchtable.get(ir):
@@ -150,3 +201,5 @@ class CPU:
             else:
                 print("Unknown instructions")
                 sys.exit(1)
+        
+            #self.trace()
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -7,11 +7,20 @@ from cpu import *
 
 cpu = CPU()
 
-# cpu.load()
-# cpu.run()
-
 if len(sys.argv) > 1:
     cpu.load(sys.argv[1])
     cpu.run()
 else:
     None
+
+# def main(argv):
+#     """Main."""
+#     if len(argv) != 2:
+#         print(f"usage: {argv[0]} filename", file=sys.stderr)
+#         return 1
+#     cpu = CPU()
+#     cpu.load(argv[1])
+#     cpu.run()
+#     return 0
+# if __name__ == "__main__":
+#     sys.exit(main(sys.argv))
